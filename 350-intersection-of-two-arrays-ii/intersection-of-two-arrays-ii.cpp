7f9a40763317f84720e4705830855760605026ad
Aryan Kumar <143013442+aryansingh0059@users.noreply.github.com>
2026-02-23 19:40:48 +0530

    Time: 1 ms (40.33%) | Memory: 15.1 MB (5.55%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mp;
+        vector<int> result;
+
+        for (int num : nums1) {
+            mp[num]++;
+        }
+        for (int num : nums2) {
+            if (mp[num] > 0) {
+                result.push_back(num);
+                mp[num]--;
+            }
+        }
+
+        return result;
+    }
+};
